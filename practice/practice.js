d427576de0b77533b1059e26b625a8e9e66f33c6
Ford Chope <douglas.f.chope@vanderbilt.edu>
2019-05-10 14:58:04 -0500

    buttons

--- practice/practice.js
+++ practice/practice.js
@@ -1,4 +1,9 @@
 $(document).ready(function() {
 // your javascript and jQuery code goes below
+console.log("whaddup yo");
+$("#push").click(function(){
+	console.log("you clicked me");
+	$("#container").css("background-color","blue");
 
+});
 })
--- script.js
+++ script.js
@@ -1,4 +1,41 @@
 $(document).ready(function() {
 // your javascript and jQuery code goes below
+var border = $("#container");
+$("#pink").click(function(){
+	console.log("you clicked me");
+	border.css({"border-color": "pink", "border-style": "dotted", "border-width": "20px"});
+});
+
+$("#blue").click(function(){
+	border.css("background-color", "#052f72");
+});
+
+$("#fade").click(function(){
+	border.fadeOut(4000);
+});
+$("#lottery").click(function(){
+	alert("YOU WIN THE LOTTERY");
+});
+$("#confirm").click(function(){
+	alert("you sure?");
+});
+$("#show").click(function(){
+	$("body").css({"background-image": "url('images/puppy.jpg')", "background-size": "cover"});
+});
+
+$("#replace").click(function(){
+	$("#replace").replaceWith("REPLACED!");
+});
+$("#old").click(function(){
+	var drive = prompt("how old are you?");
+	if (drive >= 17){
+		border.replaceWith("WOOOOHOOOO");
+	} else{
+		border.replaceWith("sorry, maybe next year");
+	}
+});
+$("#choice").click(function(){
+	border.fadeOut(4000);
+});
 
 })
