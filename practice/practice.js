05a5580b0d1eafb868a8370db183a3cfe94f23fb
aemiller2013 <alexandra.e.miller@vanderbilt.edu>
2019-05-13 11:15:33 -0500

    completed task

--- practice/practice.js
+++ practice/practice.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
-// your javascript and jQuery code goes below
+	// your javascript and jQuery code goes below
+	console.log("The page is loaded");
+	$("#clicky").click(function(){
+		//do something when the button is clicked
+		console.log("I've been clicked!");
+		$("#container").css("background-color","blue");
 
+	});
 })
--- script.js
+++ script.js
@@ -1,4 +1,48 @@
 $(document).ready(function() {
 // your javascript and jQuery code goes below
+$("#colorItPink").click(function(){
+	$(this).css("border-width", "20px");
+	$(this).css("border-color", "pink");
+});
+
+//feeling blue
+$("#feelingBlue").click(function(){
+	$("#container").css("background-color","#83b1fc");
+});
+
+//fade away
+$("#fadeAway").click(function(){
+	$("#container").fadeOut("slow");
+});
+
+//lottery button
+$("#lotteryButton").click(function(){
+	alert("YOU WON THE LOTTERY!");
+});
+
+//confirmation
+$("#confirmation").click(function(){
+	confirm("Are you sure?");
+})
+
+//show my a puppy
+$("#showMeAPuppy").click(function(){
+	$("body").css('background-image', 'url("images/puppy.jpg")');
+});
+
+//replace me
+$("#replaceMe").click(function(){
+	$(this).replaceWith("<h2>REPLACED!</h2>");
+});
+
+//old enough to drive
+$("#oldEnoughToDrive").click(function(){
+	var userAnswer =prompt("How old are you?");
+	if(userAnswer>=16){
+		$("#container").replaceWith("<h1>WOHOOO!!!</h1>");
+	}else{
+		$("#container").replaceWith("<h1>Sorry, maybe next year</h1>");
+	}
+});
 
 })
